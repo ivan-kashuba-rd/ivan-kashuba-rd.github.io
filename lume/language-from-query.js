3abe29d798aa7c88491bb623deba709c06d3ed8e
Ivan Kashuba <ivan.med.tr@gmail.com>
2026-07-18 22:36:05 +0500

    portfolio-language-fix-v2

--- lume/language-from-query.js
+++ lume/language-from-query.js
@@ -3,14 +3,20 @@
   if (!["ru", "tr", "en", "ua"].includes(lang)) return;
 
   window.addEventListener("load", () => {
-    window.setTimeout(() => {
+    let attempts = 0;
+    const timer = window.setInterval(() => {
       const select = [...document.querySelectorAll("select")].find((item) =>
         [...item.options].some((option) => option.value === "en") &&
         [...item.options].some((option) => option.value === "ru")
       );
-      if (!select) return;
-      select.value = lang;
-      select.dispatchEvent(new Event("change", { bubbles: true }));
-    }, 250);
+      if (select) {
+        select.value = lang;
+        select.dispatchEvent(new Event("change", { bubbles: true }));
+      }
+      attempts += 1;
+      if (document.documentElement.lang === lang || attempts >= 30) {
+        window.clearInterval(timer);
+      }
+    }, 200);
   });
 })();
